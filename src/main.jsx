5899f0a9c9e40136ea237f804c49fe21f01e6b21
Sarah Borgelt <borgelt.sarah@gmail.com>
2026-07-13 13:07:19 -0400

    Updating html and main. final updates before deployment. May want to review performance. While still performing well, it could be further optimized

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { HelmetProvider } from "react-helmet-async";
+import '../public/css/global.css';
 
 createRoot(document.getElementById('root')).render(
   <HelmetProvider>
